9b5062037578282d470109f9d528e18adfe275f7
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2026-04-08 22:57:54 +0200

    feat: add new SVG icons for navigation and update Navbar component
    feat: implement routing for SignIn, SignUp, Dashboard, Transactions, Analytics, Profile, and Settings pages
    
    style: enhance sidebar styling and layout for better user experience
    
    chore: remove unused assets and update index.css for new design
    
    feat: create new pages for Analytics, Dashboard, Profile, Settings, SignIn, SignUp, and Transactions

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,10 +1,33 @@
-import Navbar from "./components/Navbar"
+import { Route, Routes } from 'react-router'
+import './index.css'
+import Navbar from './components/Navbar'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
+import DashboardPage from './pages/DashboardPage'
+import TransactionsPage from './pages/TransactionsPage'
+import AnalyticsPage from './pages/AnalyticsPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+
 
 function App() {
   return (
-    <div>
+    <>
+      <Routes>
+        <Route path='/' element={<SignInPage />} />
+        <Route path='/signUp' element={<SignUpPage />} />
+      </Routes>
       <Navbar />
-    </div>
+      <main>
+        <Routes>
+          <Route path='/dashboard' element={<DashboardPage />} />
+          <Route path='/transactions' element={<TransactionsPage />} />
+          <Route path='/analytics' element={<AnalyticsPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/settings' element={<SettingsPage />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 
---
+++ website/src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni earum modi necessitatibus molestias, laboriosam optio distinctio aspernatur dolorem reprehenderit delectus voluptates illum laborum ipsam a alias fugiat facere eum dolorum.</p>
+    </div>
+  )
+}
+
+export default DashboardPage
