35b92d96e8e0e9857e683ca5300b4dac7e714362
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2026-05-13 18:08:44 +0200

    feat: enhance dashboard charts with mobile slider and responsive styles

--- website/src/pages/DashboardPage.jsx
+++ website/src/pages/DashboardPage.jsx
@@ -38,6 +38,11 @@ function DashboardPage() {
   if(isLoading || isDashboardDataLoading || isCategoriesLoading) return <div><h1>Welkom: Laden...</h1></div>;
   if(isError || isDashboardDataError || isCategoriesError) return <div>Error met laden van gebruikersgegevens</div>;
 
+  const [mobileChartIndex, setMobileChartIndex] = useState(0);
+  const nextChart = () => {setMobileChartIndex((prev) => (prev === 1 ? 0 : prev + 1));};
+  const prevChart = () => {setMobileChartIndex((prev) => (prev === 0 ? 1 : prev - 1));};
+
+
   return (
     <>
     <div className="top-container">
@@ -63,26 +68,89 @@ function DashboardPage() {
         <AmountCard type="expense" value={dashboardData?.summary.totalExpenses} />
         <AmountCard type="savings" value={dashboardData?.summary.totalSavings} />
       </div>
-      <div className="charts-container">
+      {/* <div className="charts-container">
         <div className="graph-chart">
           <div className="graph-top">
             <h2 className="title">Inkomsten vs Uitgaven</h2>
-            <button onClick={() => setAmountOfMonths(6)}>6 M</button>
-            <button onClick={() => setAmountOfMonths(12)}> 12 M </button>
+            <div className="btn-container">
+              <button className={amountOfMonths === 6 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(6)}>6 M</button>
+              <button className={amountOfMonths === 12 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(12)}> 12 M </button>
+            </div>
+            
           </div>
           {amountOfMonths === 6 ? (<MonthlyBarChart data={dashboardData?.monthlyChart.slice(6) || []}/>) : (<MonthlyBarChart data={dashboardData?.monthlyChart || []} />)}
         </div>
-
         <div className="pie-chart">
-          <h2>Uitgaven per categorie</h2>
-          <button onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
-            {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
-          </button>
+          <div className="graph-top">
+            {showCurrentMonth ? <h2 className="title">Uitgaven {currentMonth}</h2> : <h2 className="title">Uitgaven per categorie</h2>}
+            <button className="month-btn active" onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
+              {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
+            </button>
+          </div>
           <ExpensePieChart
               expenseData={ showCurrentMonth ? currentMonthExpenses : dashboardData?.expenseCategories || [] }
               categories={categories || []}
             />
         </div>
+      </div> */}
+      {/* DESKTOP CHARTS */}
+      <div className="charts-container">
+        <div className="graph-chart">
+          <div className="graph-top">
+            <h2 className="title">Inkomsten vs Uitgaven</h2>
+            <div className="btn-container">
+              <button className={amountOfMonths === 6 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(6)}> 6 M </button>
+              <button className={amountOfMonths === 12 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(12)}> 12 M </button>
+            </div>
+          </div>
+          {amountOfMonths === 6 ? (<MonthlyBarChart data={dashboardData?.monthlyChart.slice(6) || []} />) : (<MonthlyBarChart data={dashboardData?.monthlyChart || []} />)}
+        </div>
+        <div className="pie-chart">
+          <div className="graph-top">
+            {showCurrentMonth ? ( <h2 className="title">Uitgaven {currentMonth}</h2>) : (<h2 className="title">Uitgaven per categorie</h2>)}
+            <button className="month-btn active" onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
+              {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
+            </button>
+          </div>
+          <ExpensePieChart
+            expenseData={ showCurrentMonth ? currentMonthExpenses : dashboardData?.expenseCategories || [] }
+            categories={categories || []}
+          />
+        </div>
+      </div>
+
+      {/* MOBILE CHART SLIDER */}
+      <div className="mobile-chart-wrapper">
+        <div className="mobile-chart-top">
+          <button className="chart-arrow" onClick={prevChart}>{"<"}</button>
+          <button className="chart-arrow" onClick={nextChart}>{">"}</button>
+        </div>
+        {mobileChartIndex === 0 ? (
+          <div className="graph-chart">
+            <div className="graph-top">
+              <h2>Inkomsten vs Uitgaven</h2>
+              <div className="btn-container">
+                <button className={amountOfMonths === 6 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(6)}> 6 M </button>
+                <button className={amountOfMonths === 12 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(12)}> 12 M </button>
+              </div>
+            </div>
+            {amountOfMonths === 6 ? (<MonthlyBarChart data={dashboardData?.monthlyChart.slice(6) || []} />) : (<MonthlyBarChart data={dashboardData?.monthlyChart || []} />)}
+          </div>
+        ) : (
+          <div className="pie-chart">
+            <div className="graph-top">
+              {showCurrentMonth ? (<h2>Uitgaven {currentMonth}</h2>) : (<h2>Uitgaven per categorie</h2>)}
+
+              <button className="month-btn active" onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
+                {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
+              </button>
+            </div>
+            <ExpensePieChart
+              expenseData={ showCurrentMonth ? currentMonthExpenses : dashboardData?.expenseCategories || [] }
+              categories={categories || []}
+            />
+          </div>
+        )}
       </div>
     </div>
     <div className="recent-transactions-container">
