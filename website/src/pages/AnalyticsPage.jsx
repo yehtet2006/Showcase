431766f50181035010d5942239fcbe56058287f6
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2026-05-23 21:09:55 +0200

    feat(tests): enhance bearer token tests with detailed comments and structure

--- website/src/pages/AnalyticsPage.jsx
+++ website/src/pages/AnalyticsPage.jsx
@@ -1,8 +1,113 @@
-import React from 'react'
+import { useAuth } from "@clerk/react"
+import { useUser } from "../hooks/useUsers";
+import { data, Link, useNavigate } from "react-router";
+import MonthlyBarChart from "../components/MonthlyBarChart";
+import ExpensePieChart from "../components/ExpenseChart";
+import '../styles/dashboardPage.css'
+import { useDashboardData } from "../hooks/useTransactions";
+import { useCategories } from "../hooks/useCategories";
+import { useState } from "react";
 
 function AnalyticsPage() {
+  const {userId} = useAuth();
+
+  const today = new Date().toLocaleString('en-US', { month: 'short', year: 'numeric',});
+  const [amountOfMonths, setAmountOfMonths] = useState(6);
+  const [showCurrentMonth, setShowCurrentMonth] = useState(true);
+  const [mobileChartIndex, setMobileChartIndex] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState(today);
+  
+  const currentMonth = new Date().toLocaleString('nl', {
+    month: 'short',
+    year: 'numeric',
+  });
+
+  
+
+  const { data: dashboardData, isLoading: isDashboardDataLoading, isError: isDashboardDataError } = useDashboardData();
+  const { data: categories, isLoading: isCategoriesLoading, isError: isCategoriesError } = useCategories();
+  
+  const currentMonthExpenses = dashboardData?.expenseCategoriesPerMonth?.map((category) => {
+    const monthData = category.monthlyData?.find((data) => data.month === selectedMonth)
+    return {
+      category: category.category,
+      amount: monthData ? monthData.amount : 0,
+    }
+  }) || [];
+
+  if(isDashboardDataLoading ) return <div><h1>Analytics: Laden...</h1></div>;
+  if(isDashboardDataError) return <div>Error met laden van analytics gegevens</div>;
+
+  if(isCategoriesLoading) return <div><h1>Analytics: Laden...</h1></div>;
+  if(isCategoriesError) return <div>Error met laden van categorieën</div>;
+
+  const nextChart = () => {setMobileChartIndex((prev) => (prev === 1 ? 0 : prev + 1));};
+  const prevChart = () => {setMobileChartIndex((prev) => (prev === 0 ? 1 : prev - 1));};
+
+
   return (
-    <div>AnalyticsPage</div>
+    <>
+    {/* DESKTOP CHARTS */}
+      <div className="charts-container">
+        <div className="graph-chart" data-testid="income-expense-chart">
+          <div className="graph-top">
+            <h2 className="title">Inkomsten vs Uitgaven</h2>
+            <div className="btn-container">
+              <button data-testid="month-btn-6" className={amountOfMonths === 6 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(6)}> 6 M </button>
+              <button data-testid="month-btn-12" className={amountOfMonths === 12 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(12)}> 12 M </button>
+            </div>
+          </div>
+          {amountOfMonths === 6 ? (<MonthlyBarChart data={dashboardData?.monthlyChart.slice(6) || []} />) : (<MonthlyBarChart data={dashboardData?.monthlyChart || []} />)}
+        </div>
+        <div className="pie-chart" data-testid="expense-pie-chart">
+          <div className="graph-top">
+            {showCurrentMonth ? ( <h2 className="title">Uitgaven {currentMonth}</h2>) : (<h2 className="title">Uitgaven per categorie</h2>)}
+            <button data-testid="show-all-expenses-btn" className="month-btn active" onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
+              {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
+            </button>
+          </div>
+          <ExpensePieChart
+            expenseData={ showCurrentMonth ? currentMonthExpenses : dashboardData?.expenseCategories || [] }
+            categories={categories || []}
+          />
+        </div>
+      </div>
+
+      {/* MOBILE CHART SLIDER */}
+      <div className="mobile-chart-wrapper">
+        <div className="mobile-chart-top">
+          <button className="chart-arrow" onClick={prevChart}>{"<"}</button>
+          <button className="chart-arrow" onClick={nextChart}>{">"}</button>
+        </div>
+        {mobileChartIndex === 0 ? (
+          <div className="graph-chart">
+            <div className="graph-top">
+              <h2>Inkomsten vs Uitgaven</h2>
+              <div className="btn-container">
+                <button className={amountOfMonths === 6 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(6)}> 6 M </button>
+                <button className={amountOfMonths === 12 ? "month-btn active" : "month-btn"} onClick={() => setAmountOfMonths(12)}> 12 M </button>
+              </div>
+            </div>
+            {amountOfMonths === 6 ? (<MonthlyBarChart data={dashboardData?.monthlyChart.slice(6) || []} />) : (<MonthlyBarChart data={dashboardData?.monthlyChart || []} />)}
+          </div>
+        ) : (
+          <div className="pie-chart">
+            <div className="graph-top">
+              {showCurrentMonth ? (<h2>Uitgaven {currentMonth}</h2>) : (<h2>Uitgaven per categorie</h2>)}
+
+              <button data-testid="show-all-expenses-btn" className="month-btn active" onClick={() => setShowCurrentMonth(!showCurrentMonth)}>
+                {showCurrentMonth ? "Toon alle uitgaven" : "Toon huidige maand"}
+              </button>
+            </div>
+            <ExpensePieChart
+              expenseData={ showCurrentMonth ? currentMonthExpenses : dashboardData?.expenseCategories || [] }
+              categories={categories || []}
+            />
+          </div>
+        )}
+      </div>
+    </>
+    
   )
 }
 
--- website/src/tests/bearerToken.test.jsx
+++ website/src/tests/bearerToken.test.jsx
@@ -4,10 +4,12 @@ import { vi, expect, describe, it } from "vitest";
 const useMock = vi.fn();
 const ejectMock = vi.fn();
 
+// Mock the necessary modules and functions
 vi.mock("@clerk/react", () => ({
   useAuth: vi.fn(),
 }));
 
+// Mock the axios module and its interceptors
 vi.mock("../lib/axios", () => ({
   default: {
     interceptors: {
@@ -20,9 +22,10 @@ vi.mock("../lib/axios", () => ({
 }));
 
 describe("Bearer token", () => {
-  it("adds Authorization: Bearer token", async () => {
-    const { useAuth } = await import("@clerk/react");
 
+  it("adds Authorization: Bearer token", async () => {
+
+    const { useAuth } = await import("@clerk/react");
     const getTokenMock = vi.fn().mockResolvedValue("abc123");
 
     useAuth.mockReturnValue({
@@ -36,14 +39,14 @@ describe("Bearer token", () => {
     renderHook(() => useAuthReq());
 
     // interceptor function that was registered
-    const interceptorFn = useMock.mock.calls[0][0];
+    const interceptorFn = useMock.mock.calls[0][0]; // Return the first argument of the first call to useMock, which is the interceptor function
 
-    const config = { headers: {} };
+    const config = { headers: {} }; // Mock Axios request config
 
-    const result = await interceptorFn(config);
+    const result = await interceptorFn(config); // Call the interceptor function with the mock config
 
-    expect(getTokenMock).toHaveBeenCalled();
-    expect(result.headers.Authorization).toBe("Bearer abc123");
-    console.log(result.headers);
+    expect(getTokenMock).toHaveBeenCalled(); // Ensure getToken was called to retrieve the token
+    expect(result.headers.Authorization).toBe("Bearer abc123"); // Check that the Authorization header was set correctly
+    console.log(result.headers); // Log the headers to verify the output
   });
 });
