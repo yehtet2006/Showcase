3e1259673fa5712fad8b1c4d9e095c71947cfa0b
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2026-06-14 00:31:33 +0200

    feat: add cron job for health check and update dependencies

--- businesslogic/src/server.ts
+++ businesslogic/src/server.ts
@@ -1,6 +1,11 @@
 import app from "./app";
 import { ENV } from "./config/env";
+import  jobs from "./lib/cron";
 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
+
+  if(ENV.NODE_ENV === "development") {
+    jobs.start();
+  }
 });
